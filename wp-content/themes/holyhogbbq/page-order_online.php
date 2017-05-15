66c933d7b8a5f5ac14c7f94147a9aa880b47f08d
eddt <etwilbeck@marketingincolor.com>
2017-05-15 15:11:56 -0500

    removal of urban spoon

--- wp-content/themes/holyhogbbq/page-order_online.php
+++ wp-content/themes/holyhogbbq/page-order_online.php
@@ -53,7 +53,7 @@ get_header(); ?>
 
 			<div class="location medium-4 large-4 columns">
 				<div class="social-callout"><div class="ico-arrow-right"></div><a href="<?php the_field( 'yelp', 'option' ); ?>"><div class="ico-yelp-logo"></div></a></div>
-				<div class="social-callout"><div class="ico-arrow-right"></div><a href="<?php the_field( 'urbanspoon', 'option' ); ?>"><div class="ico-urbanspoon-logo"></div></a></div>
+				<!--<div class="social-callout"><div class="ico-arrow-right"></div><a href="<?php the_field( 'urbanspoon', 'option' ); ?>"><div class="ico-urbanspoon-logo"></div></a></div>-->
 				<div class="social-callout"><div class="ico-arrow-right"></div><a href="<?php the_field( 'tripadvisor', 'option' ); ?>"><div class="ico-trip-advisor-logo"></div></a></div>
 			</div>
 		</div><!-- /.row -->
